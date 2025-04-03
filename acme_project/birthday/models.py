7c399697c669cf6943ee3e9074128d19768b8502
EX0DUS28 <kapkapkap56@gmail.com>
2025-04-03 22:09:36 +0300

    initial commit

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,54 @@
+# birthday/models.py
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+# Импортируется функция-валидатор.
+from .validators import real_age
+
+
+class Tag(models.Model):
+    tag = models.CharField('Тег', max_length=20)
+
+    def __str__(self):
+        return self.tag
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', max_length=20, help_text='Необязательное поле', blank=True
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        help_text='Удерживайте CTRL для выбора нескольких вариантов'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def get_absolute_url(self):
+        return reverse("birthday:detail", kwargs={"pk": self.pk})
+
+
+class Congratulations(models.Model):
+    text = models.TextField('Текст поздравления')
+    birthday = models.ForeignKey(
+        Birthday,
+        on_delete=models.CASCADE,
+        related_name='congratulations',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created_at',)
+
